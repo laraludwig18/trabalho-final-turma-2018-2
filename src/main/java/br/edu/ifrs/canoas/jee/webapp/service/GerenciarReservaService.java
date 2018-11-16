5f267c372680d6cc5a09b20d10d84cf9de18018e
MiguelRebelatto <miguelgr199@hotmail.com>
2018-11-16 11:21:23 -0200

    Criados GerenciarReservaService, ReservaDAO, Reserva.

---
+++ src/main/java/br/edu/ifrs/canoas/jee/webapp/service/GerenciarReservaService.java
@@ -0,0 +1,33 @@
+package br.edu.ifrs.canoas.jee.webapp.service;
+
+import javax.inject.Inject;
+
+import br.edu.ifrs.canoas.jee.webapp.model.dao.ReservaDAO;
+import br.edu.ifrs.canoas.jee.webapp.model.entity.Reserva;
+
+public class GerenciarReservaService {
+	@Inject
+	private ReservaDAO reservaDAO;
+	
+	public void salvaReserva(Reserva reserva) {
+		
+		if (reserva.getId() == null) {
+			reservaDAO.insere(reserva);
+		} else {
+			reservaDAO.atualiza(reserva);
+		}
+	}
+	
+//	@SuppressWarnings("unchecked")
+//	public List<Reserva> busca(String parm) {
+//		if () {
+//			return reservaDAO.buscaPorX(parm);
+//		} else {
+//			return reservaDAO.lista();
+//		}
+//	}
+	
+	public void exclui(Reserva reserva) {
+		reservaDAO.exclui(reserva.getId());
+	}
+}
