fd21cd2c4af5008bbd99a9eaa6c5c928313851bf
laraludwig18 <laraludwig18@gmail.com>
2018-11-24 18:55:57 -0200

    refactor: Adiciona mensagens da diaria no arquivo messages e atualiza layout

--- src/main/java/br/edu/ifrs/canoas/jee/webapp/model/dao/DiariaAvulsaDAO.java
+++ src/main/java/br/edu/ifrs/canoas/jee/webapp/model/dao/DiariaAvulsaDAO.java
@@ -1,1 +1,20 @@
+package br.edu.ifrs.canoas.jee.webapp.model.dao;
 
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
+import br.edu.ifrs.canoas.jee.webapp.model.entity.DiariaAvulsa;
+import br.edu.ifrs.canoas.jee.webapp.model.entity.Quarto;
+
+public class DiariaAvulsaDAO extends BaseDAO<DiariaAvulsa, Long> {
+	private static final long serialVersionUID = 1L;
+
+	public List<DiariaAvulsa> buscaPorQuarto(Quarto quarto) {
+		TypedQuery<DiariaAvulsa> query = em.createQuery("SELECT da FROM  DiariaAvulsa da where da.quarto = :quarto",DiariaAvulsa.class);
+		query.setParameter("quarto", quarto);
+		
+		return query.getResultList();
+	}
+	
+}
