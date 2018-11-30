a8e93306ea5444c147aa933fe92d12249b4f7ce8
MiguelRebelatto <miguelgr199@hotmail.com>
2018-11-30 10:48:28 -0200

    Criado DiariaReservadaDAO; Atualização com DiariaReservada em GerenciarDiariaService; Atualização GerenciarReservaMB.

---
+++ src/main/java/br/edu/ifrs/canoas/jee/webapp/model/dao/DiariaReservadaDAO.java
@@ -0,0 +1,7 @@
+package br.edu.ifrs.canoas.jee.webapp.model.dao;
+
+import br.edu.ifrs.canoas.jee.webapp.model.entity.DiariaReservada;
+
+public class DiariaReservadaDAO extends BaseDAO<DiariaReservada, Long> {
+	private static final long serialVersionUID = 1L;
+}
