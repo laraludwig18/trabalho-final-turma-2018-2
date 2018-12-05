d9ef44fb282f475ec4e1239d4fc810d08f282b92
Mauricio Blum <mauriciocblum@gmail.com>
2018-12-05 00:59:03 -0200

    Corrigido internacionalização de Situação no Quarto

--- src/main/java/br/edu/ifrs/canoas/jee/webapp/model/entity/SituacaoQuarto.java
+++ src/main/java/br/edu/ifrs/canoas/jee/webapp/model/entity/SituacaoQuarto.java
@@ -2,16 +2,7 @@ package br.edu.ifrs.canoas.jee.webapp.model.entity;
 
 public enum SituacaoQuarto {
 	
-	DISPONIVEL("Disponível"), INDISPONIVEL("Indisponível");
+	DISPONIVEL, INDISPONIVEL;
 	
-	private String descricao;
-	
-    SituacaoQuarto(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
 
 }
